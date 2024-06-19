e9fda5a43ee32b8a5d971df610f0c98729a8856e
Jadline <njerijadline@gmail.com>
2024-06-19 12:57:02 +0000

    updating changes

--- script.js
+++ script.js
@@ -4,3 +4,26 @@
 // const body = document.body;
 const title = document.getElementById('main-heading');
 console.log(title);
+
+//GetElementByClassName()
+const listitem = document.getElementsByClassName('list-items');
+console.log(listitem);
+
+
+//GetElementByTagName
+
+const my_li = document.getElementsByTagName('li');
+console.log(my_li)
+
+
+//querySelector()
+//selects the first item ggiven of the query selectors 
+ const  container = document.querySelector('div');
+ console.log(container);
+
+ const containers = document.querySelectorAll('div');
+ console.log(containers);
+
+
+
+//querySelectorAll()
